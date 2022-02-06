ac436deafe6c5073d8a5c131ab1e77c96619b3b7
Brian Yu <brianyu187@gmail.com>
2022-02-06 16:12:59 -0500

    8-9

--- 8.smoothing.py
+++ 8.smoothing.py
@@ -3,5 +3,21 @@ import cv2 as cv
 img = cv.imread('Resources/Photos/park.jpg')
 cv.imshow('Boston', img)
 
+# averaging smoothing: take average of surrounding kernel grids
+avg = cv.blur(img, (3, 3))
+cv.imshow('Average Blur', avg)
+
+# Gauissian blur
+gaussian = cv.GaussianBlur(img, (3, 3), 0)
+cv.imshow('Gauissian Blur', gaussian)
+
+# median blur
+median = cv.medianBlur(img, 3)
+cv.imshow('Median Blur', median)
+
+# Bilateral blur
+bilateral = cv.bilateralFilter(img, 10, 35, 25)
+cv.imshow('Bilateral Blur', bilateral)
 
 cv.waitKey(0)
+ 
